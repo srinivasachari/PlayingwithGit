25c961deff1230e1c35259f18d33c951304306dd
srinivasachari <rachagundlasrinivas@gmail.com>
2025-12-10 14:32:16 +0530

    testing feature branch srinivas
    its working well

--- src/main/java/playwithGit/PlayingwithGit/PlayingwithGitApplication.java
+++ src/main/java/playwithGit/PlayingwithGit/PlayingwithGitApplication.java
@@ -9,6 +9,7 @@ public class PlayingwithGitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlayingwithGitApplication.class, args);
 		System.out.println("hello");
+		System.out.println("testing feature/srinivas branchs");
 	}
 
 }
